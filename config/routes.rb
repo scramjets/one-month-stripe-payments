5008ace63c18137d5ec391dbe43f55a2c65966f8
scramjets <wjmarcelo@gmail.com>
2016-04-12 15:45:47 -0700

    Added the ability to store purchases and display your purchase

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   # Support Stripe payments through charges
-  resources :charges 
+  resources :charges
+
+  # Route for showing purchases
+  resources :purchases, only: [:show]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
