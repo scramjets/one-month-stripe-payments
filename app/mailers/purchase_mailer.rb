dc50bdf91120b0177cd6062803c6f7a31757ffce
scramjets <wjmarcelo@gmail.com>
2016-04-21 00:39:48 -0700

    Reworked views to use product information from the database

--- app/mailers/purchase_mailer.rb
+++ app/mailers/purchase_mailer.rb
@@ -3,6 +3,7 @@ class PurchaseMailer < ActionMailer::Base
   default from: "One Month Stripe Payments <payments@onemonth.com>"
 
   def purchase_receipt purchase
+    @product = Product.find(purchase.product_id)
     @purchase = purchase
     mail to: purchase.email, subject: "Thanks for your purchase!"
   end
---
+++ db/migrate/20160421043741_adding_fieldsto_product.rb
@@ -0,0 +1,7 @@
+class AddingFieldstoProduct < ActiveRecord::Migration
+  def change
+    add_column :products, :length, :string
+    add_column :products, :author_description, :text
+    add_column :products, :author_image_name, :string
+  end
+end
