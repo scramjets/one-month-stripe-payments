a1c32b58e8174cedb7e44dce49f2075238dc761d
scramjets <wjmarcelo@gmail.com>
2016-04-21 01:51:27 -0700

    Removed seed data for single purchase growth hacking product

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,21 +7,6 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 AdminUser.create!(email: 'branch@example.com', password: 'password', password_confirmation: 'password')
 
-growth_hacking = Product.create(title: "Growth Hacking",
-  subtitle: "Crash Course", author: "Mattan Griffel", author_image_name: "teacher-image.jpg", price: "4.99", download_url: "https://s3.amazonaws.com/scramjet-stripe/YIDS_Boo.jpg",
-  sku: "GROHACK1", details: "You'll get one video", length: "30 minutes", description: %{  <p>A growth hacker is a rare combination: someone with the right marketing and technical skills who can come up with clever marketing hacks and also track their results.</p>
-
-    <p>In this talk, Mattan Griffel introduces you to the concept of Growth Hacking and shares his favorite tips for getting started as a growth hacker.</p>
-    <p><strong>What You'll Learn</strong></p>
-    <ul class="no-indent">
-        <li>What is a growth hacker?</li>
-        <li>The 5 stages of the user lifecycle</li>
-        <li>How to apply the Lean Marketing Framework</li>
-        <li>Resources and tools you'll need to know</li>
-    </ul>}, author_description: %{                <p>Hey, I'm Mattan Griffel, co-founder of the Y Combinator backed <a href="http://www.onemonth.com" target="_blank">One Month</a> where I teach the best selling One Month Rails and One Month Growth Hacking.</p>
-                    <p>Why take my class? In the past I've advised companies like Pepsico, Bloomberg, GM, NYSE, and JPMorgan on growth hacking best practices. I've also spoken at New York University, Cooper Union and First Round Capital. My goal is to help you and other entrepreneurs rapidly grow your business.
-                    </p>})
-
   growth_hacking_monthly = Product.create(title: "Growth Hacking Monthly", subtitle: "Ongoing Course", author: "Mattan Griffel", author_image_name: "teacher-image.jpg", price: "4.99", download_url: "https://s3.amazonaws.com/scramjet-stripe/YIDS_Boo.jpg",
     sku: "GROHACK2", details: "We'll charge you monthly for unlimited access to our mentoring team", length: "6 months", description: %{  <p>A growth hacker is a rare combination: someone with the right marketing and technical skills who can come up with clever marketing hacks and also track their results.</p>
 
